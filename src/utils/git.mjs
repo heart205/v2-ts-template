4936f288d0ab4241d0076fa5a18ef742cc2fa36e
heart <7362469@qq.com>
2022-06-09 00:25:59 +0800

    style: style code

--- src/utils/git.mjs
+++ src/utils/git.mjs
@@ -3,13 +3,13 @@
 /* eslint-disable no-undef */
 
 // 获取git提交的历史记录
-const log = await $`git log -a`;
-const authors = [];
-const reg = /Author\:\s*(.*?)\s*</g;
+const log = await $`git log -a`
+const authors = []
+const reg = /Author\:\s*(.*?)\s*</g
 while (true) {
   if (reg.test(log)) {
-    if (authors.indexOf(RegExp.$1) === -1) authors.push(RegExp.$1);
+    if (authors.indexOf(RegExp.$1) === -1) authors.push(RegExp.$1)
   } else {
-    break;
+    break
   }
 }

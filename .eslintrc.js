c100b45c684cf23b581fbbff70f868ac180a09fa
heart <7362469@qq.com>
2022-06-09 00:04:35 +0800

    chore: init commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier', 'vue'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      module: true,
+    },
+  },
+  rules: {
+    'no-useless-concat': 0,
+    'no-console': 0,
+  },
+}
